d25f1de8205b5ee1bf51e1fd0b93f2922cd3ed10
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2016-09-19 12:03:53 +0200

    trabajando con uninominal

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var functions = require('./functions.js');
+var dhondt = require('./dhondt_module.js');
+var uninominal = require('./uninominal.js');
 
 router.get('/', functions.index);
-router.get('/dhondtExample',functions.dhondtExample);
+router.get('/dhondtExample',dhondt.dhondtExample);
+router.get('/uninominalExample', uninominal.uninominalExample);
 
 module.exports = router;
