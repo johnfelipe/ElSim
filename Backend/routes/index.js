4f78b8e2b9ea544f4c1991d54d89a5401c06a8bf
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2016-09-21 10:37:55 +0200

    delete route

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var functions = require('./functions.js');
-var instant_runoff = require('./instant_runoff.js');
 
 router.get('/', functions.index);
 
