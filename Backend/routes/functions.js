2f98e07a2639bb7f3e3108ab80a3cccacdbf7ffe
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2016-09-21 12:53:31 +0200

    hare

--- Backend/routes/functions.js
+++ Backend/routes/functions.js
@@ -1,3 +1,9 @@
+/**
+ * Ruta para el index de la web.
+ * @param req
+ * @param res
+ * @param next
+ */
 exports.index = function(req, res, next) {
 	res.render('index',{
 		title: 'Ohh!'
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -4,8 +4,9 @@ var functions = require('./functions.js');
 var api = require('./api.js');
 
 router.get('/', functions.index);
+
 router.get('/api/', api.alive);
-
-
+router.get('/api/add', api.add);
+router.post('/api/add', api.add);
 
 module.exports = router;
