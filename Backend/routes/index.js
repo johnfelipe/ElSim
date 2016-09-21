2d2d9b2a2f329aeed14904074d8fd8f09b80217c
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2016-09-21 11:19:09 +0200

    JSDOCingggg

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var functions = require('./functions.js');
+var api = require('./api.js');
 
 router.get('/', functions.index);
+router.get('/api/', api.alive);
+
+
 
 module.exports = router;
