46f24582b45d69d00d06f799879aa77831b9d84a
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2017-01-11 09:55:07 +0100

    fix script

--- public/add-data.js
+++ public/add-data.js
@@ -1,7 +1,7 @@
 function parseFields(done) {
     $('#cod_provinciaSpan').text($('#province').val());
     $('#poblacionSpan').text($('#population').val());
-    $('#num_mesasSpan').text($('#tables').val());
+    $('#num_mesasSpan').text($('#num_mesas').val());
     $('#total_censo_electoralSpan').text($('#census').val());
     $('#total_votantesSpan').text($('#voters').val());
     $('#votos_validosSpan').text();
@@ -14,7 +14,7 @@ function parseFields(done) {
 
 function fixDate(date) {
     if (date != undefined) {
-        var fixed = date.toString();
+        let fixed = date.toString();
         fixed = fixed.substring(5, 7) + '/' + fixed.substring(0, 4);
         return fixed;
     } else {
@@ -27,16 +27,16 @@ function calculateCandidatureVotes(){
 }
 
 function processTextarea() {
-    var lines = $('#votes').val().split('\n');
+    let lines = $('#votes').val().split('\n');
     $('#partidosSpan').text('[');
-    var total = 0;
-    for (var i = 0; i < lines.length; i++) {
+    let total = 0;
+    for (let i = 0; i < lines.length; i++) {
         if(i > 0)  $('#partidosSpan').text($('#partidosSpan').text() + ', ' + lines[i]);
         else $('#partidosSpan').text($('#partidosSpan').text() + ' ' + lines[i]);
         total += +lines[i].split(' ')[2];
     }
     $('#partidosSpan').text($('#partidosSpan').text() + ']');
-    var n = +$('#votos_blancoSpan').text() + +$('#votos_nuloSpan').text();
+    let n = +$('#votos_blancoSpan').text() + +$('#votos_nuloSpan').text();
     n = n + total;
     $('#votos_validosSpan').text(n);
     $('#votos_candidaturasSpan').text(calculateCandidatureVotes());
