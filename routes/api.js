53d83ab9e5ec0094661d1474165b2e44b58eef7b
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2017-02-06 13:07:40 +0100

    api routes v1

--- routes/api.js
+++ routes/api.js
@@ -63,33 +63,33 @@ let authenticate = function (req, res) {
     }
 };
 module.exports = function () {
-    router.post('/authenticate', authenticate);
-    router.get('/', API.apiWelcome);
+    router.post('/v1/authenticate', authenticate);
+    router.get('/v1/', API.apiWelcome);
 
-    router.get('/setup', API.setup);
+    router.get('/v1/setup', API.setup);
 
     /* be careful, this function reset all system with a simple get route! */
-    router.get('/hardReset', API.hardReset);
+    router.get('/v1/reset', API.hardReset);
 
-    router.get('/', API.apiWelcome);
-    router.get('/loadCsv', API.loadCsv);
+    router.get('/v1/', API.apiWelcome);
+    router.get('/v1/csv', API.loadCsv);
 
-    router.get('/users/:id', API.findOneUser);
-    router.get('/users', API.findAllUsers);
-    router.get('/logs/:id', API.findOneLog);
-    router.get('/logs', API.findLogs);
-    router.get('/results/:id', API.findOneResultado);
-    router.get('/results', API.findAllResultados);
+    router.get('/v1/users/:id', API.findOneUser);
+    router.get('/v1/users', API.findAllUsers);
+    router.get('/v1/logs/:id', API.findOneLog);
+    router.get('/v1/logs', API.findLogs);
+    router.get('/v1/results/:id', API.findOneResultado);
+    router.get('/v1/results', API.findAllResultados);
 
-    router.post('/users', API.saveOneUser);
-    router.post('/results', API.saveOneResultado);
+    router.post('/v1/users', API.saveOneUser);
+    router.post('/v1/results', API.saveOneResultado);
 
-    router.put('/users/:id', API.updateOneUser);
-    router.put('/resultados/:id', API.updateOneResultado);
+    router.put('/v1/users/:id', API.updateOneUser);
+    router.put('/v1/results/:id', API.updateOneResultado);
 
-    router.delete('/users/:id', API.deleteOneUser);
-    router.delete('/logs', API.deleteAllLogs);
-    router.delete('/resultados/:id', API.deleteOneResultado);
-    router.delete('/resultados', API.deleteAllResultados);
+    router.delete('/v1/users/:id', API.deleteOneUser);
+    router.delete('/v1/logs', API.deleteAllLogs);
+    router.delete('/v1/results/:id', API.deleteOneResultado);
+    router.delete('/v1/results', API.deleteAllResultados);
     return router;
 };
