1750a8095b73d4e3cc9436ef1c92ef4d6ea840a0
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2017-03-24 13:02:11 +0100

    updated package.json

--- test/models/result.js
+++ test/models/result.js
@@ -3,16 +3,5 @@ const Result = require('../../models/result');
 const config = require('../../config');
 const chai = require('chai');
 describe('Result', () => {
-    it('find', (done) => {
-        Result.find({}, (err, data) => {
-            chai.assert(data.length !== 0);
-            done();
-        });
-    });
-    it('find by provincia wrong', (done) => {
-        Result.find({provincia: null}, (err, data) => {
-            chai.assert(err !== undefined);
-            done();
-        });
-    });
+
 });
