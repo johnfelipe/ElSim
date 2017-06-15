98a543a392c4dd484e1cdae54fdc47e7d4ac5782
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2017-06-15 21:49:14 +0200

    updated tests

---
+++ test/comparator-test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const Comparator = require('../modules/comparator');
+chai.should();
+
+describe('Comparator', () => {
+    it('fillDifferences', () => {
+        let c = new Comparator(null);
+
+        c.fillDifferences(
+            { A: 2, B: 4 },
+            { A: 1, B: 3 },
+            3.0,
+            4.0
+        );
+
+        c.differences.results[0].lhs.should.be.equal(2);
+        c.differences.results[0].rhs.should.be.equal(1);
+        c.differences.results[1].lhs.should.be.equal(4);
+        c.differences.results[1].rhs.should.be.equal(3);
+    });
+});
---
+++ test/district-test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const expect = chai.expect;
+const District = require('../modules/district');
+chai.should();
+
+
+describe('Single district', () => {
+    it('compute', () => {
+        let d = new District(
+            [340000, 280000, 160000, 60000, 15000],
+            ['A', 'B', 'C', 'D', 'D'],
+            {mandates: 7, blankVotes: 0, percentage: 3},
+            false
+        );
+
+        let result = d.compute();
+
+        result.numberOfVotes.should.be.equal(855000);
+        result.minNumberOfVotes.should.be.equal(25650);
+        result.parties.should.be.deep.equal({
+            A: 3, B: 3, C: 1, D: 0
+        });
+    });
+});
